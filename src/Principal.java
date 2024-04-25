147aff3b7e65ed45db5e704a7825e90a45edfde6
Mw4y <rodriguezja10@gmail.com>
2024-04-25 13:38:47 -0300

    Se completo el API y Respuesta de usuario Funcionando

--- src/Principal.java
+++ src/Principal.java
@@ -4,39 +4,65 @@ public class Principal {
     public static void main(String[] args) {
 
         Scanner lectura = new Scanner(System.in);
-        int opcion = 1;
+        int opcion = 0;
 
-        Monedas moneda = new Monedas();
+        ConvertirAPI consulta = new ConvertirAPI();
 
         try{
             while (opcion != 7) {
                 String menu = """
                         ***********************************************************
-                        Bienvanido al Convesior de Moneda de Alura
+                        Bienvanido al Convesor de Moneda de Alura
                                             
                                            
                         1 - Dolar ===> Pesos Argentinos
-                        7- Salir
+                        2 - Pesos Argentino ===> Dolar
+                        3 - Dolar ===> Real Brasilero
+                        4 - Real Brasilero ===> Dolar
+                        5 - Dolar ===> Pesos Colombianos
+                        6 - Pesos Colombianos ===> Dolar
+                        7 - Salir
                                             
                         Elija una opcion VALIDA:
                         ***********************************************************
                         """;
+                System.out.println(menu);
                 opcion = lectura.nextInt();
-
-
+                System.out.println("Ingrese el Valor a Convertir");
+                var valor = lectura.nextInt();
+                String base="", cambiar="";
                 switch (opcion) {
                     case 1:
-                        System.out.println("Ingrese el Valor a Converir");
-                        var valor = lectura.nextInt();
-
-                        var dolarPeso = Integer.valueOf(valor);
-                        var resultado = moneda.target_code() * moneda.conversion_rate();
-                        double principal = moneda.target_code();
-                        System.out.println("El valor de " + moneda.base_code());
-                        System.out.println("El valor converitido a USD es de:" + resultado);
-
-
+                        base = "USD";
+                        cambiar = "ARS";
+                        break;
+                    case 2:
+                        base = "ARS";
+                        cambiar = "USD";
+                        break;
+                    case 3:
+                        base = "USD";
+                        cambiar = "BRL";
+                        break;
+                    case 4:
+                        base = "BRL";
+                        cambiar = "USD";
+                        break;
+                    case 5:
+                        base = "USD";
+                        cambiar = "COP";
+                        break;
+                    case 6:
+                        base = "COP";
+                        cambiar = "USD";
+                        break;
+                    default:
+                        System.out.println("Gracias por utilizar la APP, el sistema esta finalizado");
                 }
+                Monedas moneda = consulta.convertir( base, cambiar);
+                double resultado = valor * moneda.conversion_rate();
+
+                System.out.println("El valor converitido de " +base+ " a "+cambiar+":" + resultado);
             }
         }catch (Exception e){
             System.out.println("Error al convertir, todos los servidores estan ocupados");
