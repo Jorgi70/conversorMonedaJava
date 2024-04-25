a4307760cf5864b57c9123d0f13fa052e7d53e9c
Mw4y <rodriguezja10@gmail.com>
2024-04-25 13:37:02 -0300

    Se arreglo variables y URI

--- src/ConvertirAPI.java
+++ src/ConvertirAPI.java
@@ -6,14 +6,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConvertirAPI {
-    public Monedas convertir ( int local, int cambiar, double multi){
+    public Monedas convertir ( String base, String cambiar){
 
         var API = "d18cc01bf9d8cb79ca14134b";
-        int base = 0;
-        int  target = 0;
-        double conversor = 0;
 
-        URI dirreccion = URI.create("https://v6.exchangerate-api.com/v6/"+API+"/pair/"+base+"/"+target+"/"+conversor);
+
+        URI dirreccion = URI.create("https://v6.exchangerate-api.com/v6/"+API+"/pair/"+base+"/"+cambiar+"/");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
