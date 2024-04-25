61e4643454b92b90b1989fc9611cae2a0b32a7e1
Mw4y <rodriguezja10@gmail.com>
2024-04-25 13:37:46 -0300

    En espera para ver que hacemos con variables de sobra

--- src/Monedas.java
+++ src/Monedas.java
@@ -1,4 +1,4 @@
-public record Monedas(double base_code,
-                      double target_code,
+public record Monedas(String base_code,
+                      String target_code,
                       double conversion_rate) {
 }
