1cbbcc56fcdb8735be6e781a18e3df710c116c22
Mw4y <rodriguezja10@gmail.com>
2024-04-26 04:59:20 -0300

    Se modifico el ToString

--- src/Monedas.java
+++ src/Monedas.java
@@ -1,4 +1,11 @@
 public record Monedas(String base_code,
                       String target_code,
                       double conversion_rate) {
+
+    @Override
+    public String toString() {
+        return "Moneda='" + base_code +
+               "Tipo de cambio='" + target_code +
+               "Valor=" + conversion_rate;
+    }
 }
