93da4275d6c9cf9ea24d6bdaefea67cdbcccaa47
Mw4y <rodriguezja10@gmail.com>
2024-05-19 01:35:42 -0300

    Se hizo un Comparable

--- src/Monedas.java
+++ src/Monedas.java
@@ -1,11 +1,16 @@
-public record Monedas(String base_code,
+public record Monedas (String base_code,
                       String target_code,
-                      double conversion_rate) {
+                      double conversion_rate) implements Comparable<Monedas> {
 
     @Override
     public String toString() {
-        return "Moneda='" + base_code +
-               "Tipo de cambio='" + target_code +
-               "Valor=" + conversion_rate;
+        return " Valor ='" + base_code +
+                "Conversion ='" + target_code +
+                "Precio =" + conversion_rate;
+    }
+
+    @Override
+    public int compareTo(Monedas otraMoneda) {
+        return Double.valueOf(this.conversion_rate).compareTo(Double.valueOf(otraMoneda.conversion_rate));
     }
 }
